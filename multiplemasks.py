0704b674eba1df7160abaa177c2f067c8e2032cd
horatioh13 <horatioh13@gmail.com>
2024-09-12 16:50:09 +0100

    working version of multiplemasks.py

--- multiplemasks.py
+++ multiplemasks.py
@@ -30,9 +30,73 @@ edgemode = 'multiplemasks'
 
 ############################################
 
-##### OLD FUNCTIONS #####
+def cleardata():
+    dir_path = 'original_image'
+    for file in os.listdir(dir_path):
+        if imagesource == 'file':
+            continue
+        
+        file_path = os.path.join(dir_path, file)
+        os.remove(file_path)
 
+    dir_path = 'cropped_image'
+    for file in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file)
+        os.remove(file_path)
     
+    dir_path = 'masks'
+    # Check if the directory exists
+    if os.path.exists(dir_path):
+        # Remove everything in the directory
+        for file in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, file)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+
+    dir_path = 'outlines_svg'
+    for file in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file)
+        os.remove(file_path)
+
+    dir_path = 'outlines_png'
+    for file in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file)
+        os.remove(file_path)
+       
+def get_image_from_source():
+    if imagesource == 'thispersondoesnotexist':
+        image_url = 'https://thispersondoesnotexist.com'
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            image_bytes = io.BytesIO(response.content)
+            image1 = cv2.imdecode(np.frombuffer(image_bytes.read(), np.uint8), cv2.IMREAD_COLOR)
+            cv2.imwrite('original_image/image1.png', image1)
+        except requests.RequestException as e:
+            print(f'Failed to download the image: {e}')
+
+    elif imagesource == 'laptopwebcam':
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cap.release()
+        if ret:
+            cv2.imwrite('original_image/image1.png', frame)
+        else:
+            print('Failed to capture image from webcam.')
+
+    elif imagesource == 'usbwebcam':
+        cap = cv2.VideoCapture(2)
+        ret, frame = cap.read()
+        cap.release()
+        if ret:
+            cv2.imwrite('original_image/image1.png', frame)
+        else:
+            print('Failed to capture image from webcam.')
+    
+    elif imagesource == 'file':
+        image1 = 'original_image/image1.png'
 
 def remove_background(input_path,output_path):
     if remove_model == 'rembg':
@@ -106,6 +170,16 @@ def detect_and_segment_glasses(inputpath,outputpath):
     elif ans == False:
         print('No glasses detected')
 
+def align_and_crop():
+    cropper = Cropper(strategy="largest")
+    cropper.process_dir(input_dir='original_image',output_dir='cropped_image')
+
+def segment_face():
+    cropper = Cropper(
+    mask_groups={"skin":[1],"eybrows":[2,3],"eyes":[4,5],"glasses":[6],"ears":[7,8],"earings":[9],"nose":[10],"mouth":[11],"upper lip":[12],"lower lip":[13],"neck":[14],"necklace":[15],"clothes":[16],"hair":[17],"hat":[18]},
+    )
+    cropper.process_dir(input_dir="cropped_image",output_dir="masks")
+
 def create_outlines():
     dir_path = 'masks'
     for file in os.listdir(dir_path):
@@ -118,22 +192,22 @@ def create_outlines():
             get_edges_from_mask(image_path, new_path)
 
 
-
-#remove_background('original_image/image1.png','original_image/image1.png')
-#get_edges()
-#detect_and_segment_glasses('cropped_image/image1.png','glasses.png')
-#segment_face()
-#create_outlines()
-
-#############################################################################
-                              ##### NEW FUNCTIONS #####
-#############################################################################
-
-
-
-#### helper functions ####
+#### functions for working in RAM ####
 def read_PNG_mask(image_path):
     return cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+def NUMPY_get_edges_from_mask(image):    
+    # Find contours
+    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Create an empty image to draw contours
+    contour_image = np.zeros_like(image)
+    
+    # Draw contours
+    cv2.drawContours(contour_image, contours, -1, (255, 255, 255), 1)
+    
+    # Return the resulting image
+    return contour_image
 
 def NUMPY_get_edges_from_mask2(image):
     # Create a copy of the image to modify
@@ -157,7 +231,11 @@ def NUMPY_get_edges_from_mask2(image):
 
     result_image[result_image == 127] = 0 
 
-    return result_image  
+    return result_image
+
+def NUMPY_process_bitmaps(image):
+
+    return image    
 
 def NUMPY_convert_to_SVG(image, output_path):
     # Find contours
@@ -175,104 +253,35 @@ def NUMPY_convert_to_SVG(image, output_path):
     # Save the SVG file
     dwg.save()    
 
-def dilate_and_subtract(target, source):
-    if (source is not None) and (target is not None):
-        # Create a copy of the target to modify
-        result = target.copy()
-
-        # Dilate the source image by 1 pixel
-        kernel = np.ones((3, 3), np.uint8)
-        dilated_source = cv2.dilate(source, kernel, iterations=1)
-
-        # Subtract the dilated source from the target
-        result = cv2.subtract(result, dilated_source)
-
-        return result
-    else:
-        return target
-
-
-
-##### MAIN FUNCTIONS #####
-#############################################################################
-
-def cleardata():
-    dir_path = 'original_image'
-    for file in os.listdir(dir_path):
-        if imagesource == 'file':
-            continue
-        
-        file_path = os.path.join(dir_path, file)
-        os.remove(file_path)
-
-    dir_path = 'cropped_image'
-    for file in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, file)
-        os.remove(file_path)
-    
+def mask_dir_to_svgs():
     dir_path = 'masks'
-    # Check if the directory exists
-    if os.path.exists(dir_path):
-        # Remove everything in the directory
-        for file in os.listdir(dir_path):
-            file_path = os.path.join(dir_path, file)
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-
-    dir_path = 'outlines_svg'
     for file in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file)
-        os.remove(file_path)
+        if file_path.endswith('_mask'):
+            image_path = file_path + '/image1.png'
+            name = file_path[6:-5]
+            mask = read_PNG_mask(image_path)
+            #mask = NUMPY_process_bitmaps(mask)
+            svg_path = 'outlines_svg/' + name + '_outline.svg'
+            NUMPY_convert_to_SVG(mask, svg_path)
 
-    dir_path = 'outlines_png'
-    for file in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, file)
-        os.remove(file_path)
+def combine_and_plot_masks(masks_dict):
+    combined_mask = None
+    masks = masks_dict.values()
+    for mask in masks:
+        if mask is not None:        
+            if combined_mask is None:
+                combined_mask = mask
+            else:
+                combined_mask = cv2.bitwise_or(combined_mask, mask)
 
-def get_image_from_source():
-    if imagesource == 'thispersondoesnotexist':
-        image_url = 'https://thispersondoesnotexist.com'
-        try:
-            response = requests.get(image_url)
-            response.raise_for_status()  # Raise an exception for HTTP errors
-            image_bytes = io.BytesIO(response.content)
-            image1 = cv2.imdecode(np.frombuffer(image_bytes.read(), np.uint8), cv2.IMREAD_COLOR)
-            cv2.imwrite('original_image/image1.png', image1)
-        except requests.RequestException as e:
-            print(f'Failed to download the image: {e}')
-
-    elif imagesource == 'laptopwebcam':
-        cap = cv2.VideoCapture(0)
-        ret, frame = cap.read()
-        cap.release()
-        if ret:
-            cv2.imwrite('original_image/image1.png', frame)
-        else:
-            print('Failed to capture image from webcam.')
-
-    elif imagesource == 'usbwebcam':
-        cap = cv2.VideoCapture(2)
-        ret, frame = cap.read()
-        cap.release()
-        if ret:
-            cv2.imwrite('original_image/image1.png', frame)
-        else:
-            print('Failed to capture image from webcam.')
-    
-    elif imagesource == 'file':
-        image1 = 'original_image/image1.png'
-
-def align_and_crop():
-    cropper = Cropper(strategy="largest")
-    cropper.process_dir(input_dir='original_image',output_dir='cropped_image')
-
-def segment_face():
-    cropper = Cropper(
-    mask_groups={"skin":[1],"eybrows":[2,3],"eyes":[4,5],"glasses":[6],"ears":[7,8],"earings":[9],"nose":[10],"mouth":[11],"upper lip":[12],"lower lip":[13],"neck":[14],"necklace":[15],"clothes":[16],"hair":[17],"hat":[18]},
-    )
-    cropper.process_dir(input_dir="cropped_image",output_dir="masks")
+    if combined_mask is not None:
+        # Display the combined mask using OpenCV
+        cv2.imshow('Combined Mask', combined_mask)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("No masks provided.")
 
 def mask_dir_to_NUMPY_arrays():
     dir_path = 'masks'
@@ -294,6 +303,22 @@ def mask_dir_to_NUMPY_arrays():
             mask_groups[key] = None
     
     return mask_groups
+
+def dilate_and_subtract(target, source):
+    if (source is not None) and (target is not None):
+        # Create a copy of the target to modify
+        result = target.copy()
+
+        # Dilate the source image by 1 pixel
+        kernel = np.ones((3, 3), np.uint8)
+        dilated_source = cv2.dilate(source, kernel, iterations=1)
+
+        # Subtract the dilated source from the target
+        result = cv2.subtract(result, dilated_source)
+
+        return result
+    else:
+        return target
 
 def process_NUMPY_outlines(groups):
     # Process the outlines
@@ -337,36 +362,27 @@ def process_NUMPY_outlines(groups):
 
     return groups
 
-def combine_and_plot_masks(masks_dict):
-    combined_mask = None
-    masks = masks_dict.values()
-    for mask in masks:
-        if mask is not None:        
-            if combined_mask is None:
-                combined_mask = mask
-            else:
-                combined_mask = cv2.bitwise_or(combined_mask, mask)
-
-    if combined_mask is not None:
-        # Display the combined mask using OpenCV
-        cv2.imshow('Combined Mask', combined_mask)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
-        print("No masks provided.")
-
 
 
 
 if __name__ == '__main__':
-    cleardata()
-    get_image_from_source()
-    align_and_crop()
-    
+    #cleardata()
+    #get_image_from_source()
+    #remove_background('original_image/image1.png','original_image/image1.png')
+    #get_edges()
+    #detect_and_segment_glasses('cropped_image/image1.png','glasses.png')
+
+    #align_and_crop()
+    segment_face()
+    #create_outlines()
+
+    #mask_dir_to_NUMPY_arrays()
+
+    #combine_and_plot_masks()
+
     groups = mask_dir_to_NUMPY_arrays() 
     groups2 = process_NUMPY_outlines(groups)
     combine_and_plot_masks(groups2)
-
     #NUMPY_convert_to_SVG(groups2['eybrows'], 'neck.svg')
 
     
